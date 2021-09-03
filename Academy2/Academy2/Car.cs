bbbf821050fd71f25f8f3775957250dec1b5649f
ms-darianbenito <dbenito@makingsense.com>
2021-09-03 17:41:26 -0300

    feat: add car model

---
+++ Academy2/Academy2/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Academy2
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Model { get; set; }
+        public int Doors { get; set; }
+        public string Color { get; set; }
+    }
+}
