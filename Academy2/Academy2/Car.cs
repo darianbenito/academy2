5a1be98519c1b7c74b6b87501340fdabb78b7902
matosmdq88 <matosmdq88@gmail.com>
2021-09-03 17:52:14 -0300

    Add property Brand

--- Academy2/Academy2/Car.cs
+++ Academy2/Academy2/Car.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using Academy2.Enums;
 
 namespace Academy2
 {
     public class Car
     {
         public Guid Id { get; set; }
+        public Brand Brand { get; set; }
         public string Model { get; set; }
         public int Doors { get; set; }
         public string Color { get; set; }
