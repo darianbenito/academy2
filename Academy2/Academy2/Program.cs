7608560ef9921842aac346b9b3268bb796edb872
ms-darianbenito <dbenito@makingsense.com>
2021-09-03 17:30:40 -0300

    feat: init repo

---
+++ Academy2/Academy2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Academy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
